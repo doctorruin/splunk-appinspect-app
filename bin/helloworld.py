a8a2b928f3fb8ef8500b5604cbab591427fbe4af
rabey <randolphzemail@gmail.com>
2019-03-01 16:56:27 -0800

    formatting json

--- bin/helloworld.py
+++ bin/helloworld.py
@@ -37,7 +37,7 @@ def get_report(request_id, user_token):
 
     response = requests.get(uri, headers=headers)
 
-    parsed = json.load(response.text)
+    parsed = json.loads(response.text)
 
     print (json.dumps(parsed, indent=4, sort_keys=True))
 
